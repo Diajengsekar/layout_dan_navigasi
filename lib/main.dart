3de11d2e0d297f8ff1cebee103c9bd8076f7d1fb
Diajengsekar <diajengsekararum31@gmail.com>
2025-10-04 19:36:25 +0700

    prak 2 - Langkah 1: Buat method Column _buildButtonColumn

--- lib/main.dart
+++ lib/main.dart
@@ -8,57 +8,76 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget titleSection = Container(
-  padding: const EdgeInsets.all(32), // soal 3 → padding 32
-  child: Row(
-    children: [
-      Expanded(
-        /* soal 1 */
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            /* soal 2 */
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: const Text(
-                'Wisata Gunung di Batu',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+      padding: const EdgeInsets.all(32), // soal 3 → padding 32
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1 */
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2 */
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              ),
+                const Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
             ),
-            const Text(
-              'Batu, Malang, Indonesia',
-              style: TextStyle(
-                color: Colors.grey,
-              ),
-            ),
-          ],
-        ),
+          ),
+          /* soal 3 */
+          const Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          const Text('41'),
+        ],
       ),
-      /* soal 3 */
-      const Icon(
-        Icons.star,
-        color: Colors.red,
-      ),
-      const Text('41'),
-    ],
-  ),
-);
+    );
 
-
-    // ⬇️ Gunakan di body
     return MaterialApp(
       title: 'Flutter layout: Diajeng Sekar Arum(2341760070)',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-            body: Column(
-            children: [
+        body: Column(
+          children: [
             titleSection,
           ],
         ), // ganti dari Center('Hello World')
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
