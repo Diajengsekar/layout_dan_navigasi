667a80b24fe4f3299d812d57c72e563a86474e5a
Diajengsekar <diajengsekararum31@gmail.com>
2025-10-04 20:22:51 +0700

    prak 4 - Langkah 3: Ubah menjadi ListView

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
           children: [
             Image.asset(
               'images/gunung.jpg',   
